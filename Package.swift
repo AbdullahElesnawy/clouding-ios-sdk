dd275f8ab78bec1481d7408366482413395bbade
sdk-release <sdk-release@clouding.ai>
2026-08-11 10:16:24 +0000

    Release 1.0.3

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ChatWidgetSDK",
-            url: "https://github.com/AbdullahElesnawy/clouding-ios-sdk/releases/download/1.0.0/ChatWidgetSDK-1.0.0.xcframework.zip",
-            checksum: "80cc945cd5ebe363ff80047f81db2b33f7d6acc38d6aff47fbe0988b2185894b"
+            url: "https://github.com/AbdullahElesnawy/clouding-ios-sdk/releases/download/1.0.3/ChatWidgetSDK-1.0.3.xcframework.zip",
+            checksum: "8a3ed0d7afd81124abfcdadb20ad3a6a497c58b6f099bd045428f50d5bfc2f25"
         )
     ]
 )
